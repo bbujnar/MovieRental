938829b2a0df2bd96a72a6406ddf0fd77315abc1
bbujnar <bbujnar@gmail.com>
2023-07-02 18:02:02 +0200

    Creating list of clients + filtering this list

--- MovieRentalClients.xaml.cs
+++ MovieRentalClients.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +18,92 @@ namespace MovieRental
     /// <summary>
     /// Logika interakcji dla klasy MovieRentalClients.xaml
     /// </summary>
+    
     public partial class MovieRentalClients : Window
     {
+        public string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MovieRental;Integrated Security=True";
+        private List<Client> clients;
+
         public MovieRentalClients()
         {
             InitializeComponent();
+
+            // Pobierz dane klientów z bazy danych
+            clients = GetClientsData();
+
+            // Ustaw dane klientów jako źródło dla ListBox
+            clientsListView.ItemsSource = clients;
         }
+
+        private List<Client> GetClientsData()
+        {
+            List<Client> clients = new List<Client>();
+
+            // Połączenie z bazą danych
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    // Otwarcie połączenia
+                    connection.Open();
+                    string query;
+                    // Zapytanie SQL do pobrania danych klientów
+                    query = "SELECT Klienci.IDklienta, Klienci.Imie, Klienci.Nazwisko, COUNT(*) AS LiczbaWypożyczeń FROM Wypożyczenia JOIN Klienci ON Wypożyczenia.IDklienta = Klienci.IDklienta JOIN Filmy ON Wypożyczenia.IDFilmu = Filmy.IDFilmu GROUP BY Klienci.IDklienta, Imie, Nazwisko;";
+                    
+
+                    // Wykonanie zapytania SQL
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        // Odczyt danych klientów z wyniku zapytania
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                int id = reader.GetInt32(reader.GetOrdinal("IDklienta"));
+                                string name = reader["Imie"].ToString();
+                                string surname = reader["Nazwisko"].ToString();
+                                int rentedMovies = reader.GetInt32(reader.GetOrdinal("LiczbaWypożyczeń"));
+
+                                // Tworzenie obiektu klienta na podstawie danych z bazy danych
+                                Client client = new Client { Id = id, Name = name, Surname = surname , };
+
+                                // Dodanie klienta do listy
+                                clients.Add(client);
+                            }
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"Błąd połączenia z bazą danych: {ex.Message}");
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            return clients;
+        }
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchKeyword = searchTextBox.Text.ToLower();
+
+            // Filtruj klientów na podstawie wprowadzonego tekstu
+            List<Client> filteredClients = clients.Where(c => c.Name.ToLower().Contains(searchKeyword) || c.Surname.ToLower().Contains(searchKeyword)).ToList();
+
+            // Aktualizuj źródło danych dla ListView
+            clientsListView.ItemsSource = filteredClients;
+        }
+    }
+
+
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int RentedMovies { get; set; }
     }
 }
